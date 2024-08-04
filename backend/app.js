b819c9c7656d4d5a886cf7e71a61bb53457003f9
Lu <96496642+LuluDev1@users.noreply.github.com>
2024-08-03 22:04:00 -0500

    testing

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.json());
+
+const userRoutes = require("./routes/user-routes");
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,19 +19,34 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/auth/signup", (req, res) => {
-  // Log the request body (parsed JSON data)
-  console.log(req.body);
+app.use("/api/auth", userRoutes);
 
-  // Send a response back to the client
-  res.status(201).json({ message: "User signed up successfully!" });
-});
-app.post("/api/auth/login", (req, res) => {
-  // Log the request body (parsed JSON data)
-  console.log(req.body);
+// const user = process.env.DB_USERNAME;
+// const password = process.env.DB_PASSWORD;
 
-  // Send a response back to the client
-  res.status(201).json({ message: "User Logged in!" });
-});
+// mongoose
+//   .connect(`mongodb+srv://devlulu:${password}@cluster0.9g4tbes.mongodb.net/`)
+//   .then(() => {
+//     console.log("Successfully connected to MongoDB Atlas!");
+//   })
+//   .catch((error) => {
+//     console.log("Unable to connect to MongoDB Atlas!");
+//     console.error(error);
+//   });
+
+// app.post("/api/auth/signup", (req, res) => {
+//   // Log the request body (parsed JSON data)
+//   console.log(req.body);
+
+//   // Send a response back to the client
+//   res.status(201).json({ message: "User signed up successfully!" });
+// });
+// app.post("/api/auth/login", (req, res) => {
+//   // Log the request body (parsed JSON data)
+//   console.log(req.body);
+
+//   // Send a response back to the client
+//   res.status(201).json({ message: "User Logged in!" });
+// });
 
 module.exports = app;
---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,6 @@
+exports.signup = (req, res, next) => {
+  res.status(200).send("Works");
+};
+exports.login = (req, res, next) => {
+  res.status(200).send("Works");
+};
